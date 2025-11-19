6ab8eed06fe4bee6dcf4ae80e21c26f9359fe6eb
Enea Maroncelli <ENEA.MARONCELLI@local.ittcesena.it>
2025-11-19 09:24:16 +0100

    Classe libro

--- Catalogo/Libro.cs
+++ Catalogo/Libro.cs
@@ -8,10 +8,38 @@ namespace Catalogo
 {
     internal class Libro : Prodotto
     {
+        public int Pagine { get; set; }
+        private bool _IsCopertinaRigida;
 
-        public Libro(string codiceProdotto, string titolo, decimal prezzoBase) : base(codiceProdotto, titolo, prezzoBase) 
+        public Libro(string codiceProdotto, string titolo, decimal prezzoBase,int pagine,bool rigida) : base(codiceProdotto, titolo, prezzoBase) 
         {
-        
+            this.Pagine = pagine;
+            this._IsCopertinaRigida = rigida;
+        }
+        public override decimal CalcolaPrezzoFinale()
+        {
+            if (_IsCopertinaRigida)
+            {
+                decimal sovrapprezzo = 2.5m;
+                return sovrapprezzo + base.CalcolaPrezzoFinale();
+            }
+            else
+            {
+                return base.CalcolaPrezzoFinale();
+            }
+
+        }
+        public override string OttieniDettagli()
+        {
+            return base.OttieniDettagli() + $"Numero pagine: {this.Pagine}, Copertina {(this._IsCopertinaRigida ? "rigida" : "non rigida") }";
+        }
+        public override string FormatoPerFile()
+        {
+            return base.FormatoPerFile() + ";" + this.Pagine + ";" + this._IsCopertinaRigida;
+        }
+        public bool GetRigida()
+        {
+            return _IsCopertinaRigida;
         }
     }
 }
