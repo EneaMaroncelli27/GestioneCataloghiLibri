4952205199d37b011019c79e8b21cdf43e370af4
Enea Maroncelli <ENEA.MARONCELLI@local.ittcesena.it>
2025-11-19 09:10:51 +0100

    Created all classes and concept of class prodotto

---
+++ Catalogo/Libro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogo
+{
+    internal class Libro : Prodotto
+    {
+
+        public Libro(string codiceProdotto, string titolo, decimal prezzoBase) : base(codiceProdotto, titolo, prezzoBase) 
+        {
+        
+        }
+    }
+}
---
+++ Catalogo/Prodotto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogo
+{
+    internal class Prodotto
+    {
+        private string _codiceProdotto;
+        public string Titolo { get; set; }
+        public decimal PrezzoBase { get; private set; }
+
+        protected Prodotto(string codiceProdotto, string titolo, decimal prezzoBase)
+        {
+            _codiceProdotto = codiceProdotto;
+            Titolo = titolo;
+            PrezzoBase = prezzoBase;
+        }
+        public string GetCodice()
+        {
+            return _codiceProdotto;
+        }
+        public virtual decimal CalcolaPrezzoFinale()
+        {
+            return PrezzoBase;
+        }
+        public virtual string OttieniDettagli()
+        {
+            return $"Codice Prodotto: {_codiceProdotto}, Titolo: {Titolo}, Prezzo Base: {PrezzoBase:C}";
+        }
+        public virtual string FormatoPerFile()
+        {
+            return "" + _codiceProdotto + ";" + Titolo + ";" + PrezzoBase;
+        }
+    }
+}
